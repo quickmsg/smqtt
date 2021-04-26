745efc49bc1823417e27a597c2bc1a3ec651da20
luxurong <1074455781@qq.com>
2021-04-26 23:11:57 +0800

    bootstrap update

--- smqtt-bootstrap/src/main/java/com/github/quickmsg/jar/JarStarter.java
+++ smqtt-bootstrap/src/main/java/com/github/quickmsg/jar/JarStarter.java
@@ -3,6 +3,8 @@ package com.github.quickmsg.jar;
 import com.github.quickmsg.AbstractStarter;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.print.attribute.standard.NumberUp;
+
 /**
  * @author luxurong
  * @date 2021/4/14 20:39
@@ -14,8 +16,8 @@ public class JarStarter extends AbstractStarter {
     public static void main(String[] args) {
         log.info("JarStarter start args {}", String.join(",", args));
         if (args.length > 0) {
-            start(System::getProperty, null);
-        } else {
             start(System::getProperty, args[0]);
+        } else {
+            start(System::getProperty, null);
         }    }
 }
