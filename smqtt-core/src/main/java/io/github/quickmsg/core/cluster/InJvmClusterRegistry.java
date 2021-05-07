71565aee5f5e38a817dd972645619e0ec8b87064
luxurong <1074455781@qq.com>
2021-05-08 00:20:48 +0800

    pom

--- smqtt-core/src/main/java/io/github/quickmsg/core/cluster/InJvmClusterRegistry.java
+++ smqtt-core/src/main/java/io/github/quickmsg/core/cluster/InJvmClusterRegistry.java
@@ -2,14 +2,20 @@ package io.github.quickmsg.core.cluster;
 
 import io.github.quickmsg.common.cluster.ClusterConfig;
 import io.github.quickmsg.common.cluster.ClusterMessage;
+import io.github.quickmsg.common.cluster.ClusterNode;
 import io.github.quickmsg.common.cluster.ClusterRegistry;
+import io.github.quickmsg.common.enums.ClusterEvent;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author luxurong
  */
 public class InJvmClusterRegistry implements ClusterRegistry {
+
 
     @Override
     public void registry(ClusterConfig c) {
@@ -22,8 +28,13 @@ public class InJvmClusterRegistry implements ClusterRegistry {
     }
 
     @Override
-    public <E> Flux<ClusterEvent<E>> clusterEvent() {
+    public Flux<ClusterEvent> clusterEvent() {
         return Flux.empty();
+    }
+
+    @Override
+    public List<ClusterNode> getClusterNode() {
+        return Collections.emptyList();
     }
 
     @Override
