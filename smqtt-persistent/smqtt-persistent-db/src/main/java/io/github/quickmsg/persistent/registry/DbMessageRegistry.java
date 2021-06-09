b3e995324941650ddac310536c42380b16430bb6
zhaopeng <1091927336@qq.com>
2021-06-09 15:00:52 +0800

    db test

--- smqtt-persistent/smqtt-persistent-db/src/main/java/io/github/quickmsg/persistent/registry/DbMessageRegistry.java
+++ smqtt-persistent/smqtt-persistent-db/src/main/java/io/github/quickmsg/persistent/registry/DbMessageRegistry.java
@@ -7,9 +7,18 @@ import io.github.quickmsg.common.message.SessionMessage;
 import io.github.quickmsg.persistent.DbConnectionHolder;
 import io.github.quickmsg.persistent.config.DruidConnectionProvider;
 import io.github.quickmsg.persistent.sql.SqlLoader;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.FileSystemResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+import java.io.File;
+import java.net.URL;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
@@ -27,18 +36,35 @@ public class DbMessageRegistry implements MessageRegistry {
 
     @Override
     public void startUp(EnvContext envContext) {
+        String path = DbMessageRegistry.class.getClassLoader().getResource("/dbChangelog.xml").getPath();
         Properties properties = new Properties();
         properties.putAll(envContext.getEnvironments());
         DruidConnectionProvider
                 .singleTon()
                 .init(properties)
-                .then(Mono.when(SqlLoader.loadSql().stream().map(sql ->
+             /*   .then(Mono.when(SqlLoader.loadSql().stream().map(sql ->
                         DbConnectionHolder.getDslContext().doOnNext(dslContext -> dslContext.execute(sql))
-                ).collect(Collectors.toList())))
+                ).collect(Collectors.toList())))*/
                 .subscribe();
+
+        DruidConnectionProvider.singleTon().getConnection().subscribe(conn -> {
+                    Database database = null;
+                    try {
+                        database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(conn));
+                    } catch (DatabaseException e) {
+                        e.printStackTrace();
+                    }
+                    Liquibase liquibase = new Liquibase(path, new FileSystemResourceAccessor(), database);
+                    try {
+                        liquibase.update("db");
+                    } catch (LiquibaseException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
     }
 
-   // TODO session一个表  retain 一个表
+    // TODO session一个表  retain 一个表
     @Override
     public List<SessionMessage> getSessionMessages(String clientIdentifier) {
         return null;
