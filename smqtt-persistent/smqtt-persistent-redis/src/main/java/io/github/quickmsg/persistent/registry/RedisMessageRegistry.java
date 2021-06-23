9e8629d165a8eee78b1c2db6f29f025e1d935c0e
1091927336@qq.com <1091927336@qq.com>
2021-06-23 11:15:10 +0800

    1. redis持久化模块添加try-catch 2. getRetainMessage添加正则表达式匹配

--- smqtt-persistent/smqtt-persistent-redis/src/main/java/io/github/quickmsg/persistent/registry/RedisMessageRegistry.java
+++ smqtt-persistent/smqtt-persistent-redis/src/main/java/io/github/quickmsg/persistent/registry/RedisMessageRegistry.java
@@ -5,17 +5,20 @@ import io.github.quickmsg.common.environment.EnvContext;
 import io.github.quickmsg.common.message.MessageRegistry;
 import io.github.quickmsg.common.message.RetainMessage;
 import io.github.quickmsg.common.message.SessionMessage;
+import io.github.quickmsg.common.utils.TopicRegexUtils;
 import io.github.quickmsg.persistent.factory.ClientFactory;
 import io.github.quickmsg.persistent.message.RetainMessageEntity;
 import io.github.quickmsg.persistent.message.SessionMessageEntity;
 import io.github.quickmsg.persistent.strategy.ClientStrategy;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBucket;
+import org.redisson.api.RKeys;
 import org.redisson.api.RList;
 import org.redisson.api.RedissonClient;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 
 /**
@@ -34,93 +37,114 @@ public class RedisMessageRegistry implements MessageRegistry {
 
     @Override
     public void startUp(EnvContext envContext) {
-        Map<String, String> environments = envContext.getEnvironments();
-        // 获取客户端策略
-        ClientStrategy clientStrategy = ClientFactory.getClientStrategy(environments.get(BootstrapKey.Redis.REDIS_MODE));
-        // 获取redisson客户端
-        redissonClient = clientStrategy.getRedissonClient(environments);
-
-        redissonClient.shutdown();
+        try {
+            Map<String, String> environments = envContext.getEnvironments();
+            // 获取客户端策略
+            ClientStrategy clientStrategy = ClientFactory.getClientStrategy(environments.get(BootstrapKey.Redis.REDIS_MODE));
+            // 获取redisson客户端
+            redissonClient = clientStrategy.getRedissonClient(environments);
+        } catch (Exception e) {
+            log.error("startUp error message", e);
+        }
     }
 
     @Override
     public List<SessionMessage> getSessionMessage(String clientIdentifier) {
-        RList<SessionMessageEntity> list = redissonClient.getList(BootstrapKey.Redis.REDIS_SESSION_MESSAGE_PREFIX_KEY + clientIdentifier);
-        List<SessionMessage> resList = list.stream().map(record -> SessionMessage.builder()
-                .topic(record.getTopic())
-                .clientIdentifier(record.getClientId())
-                .qos(record.getQos())
-                .retain(record.getRetain())
-                .body(record.getBody())
-                .build()
-        ).collect(Collectors.toList());
+        try {
+            RList<SessionMessageEntity> list = redissonClient.getList(BootstrapKey.Redis.REDIS_SESSION_MESSAGE_PREFIX_KEY + clientIdentifier);
+            List<SessionMessage> resList = list.stream().map(record -> SessionMessage.builder()
+                    .topic(record.getTopic())
+                    .clientIdentifier(record.getClientId())
+                    .qos(record.getQos())
+                    .retain(record.getRetain())
+                    .body(record.getBody())
+                    .build()
+            ).collect(Collectors.toList());
 
-        if (list.size() > 0) {
-            redissonClient.getBucket(BootstrapKey.Redis.REDIS_SESSION_MESSAGE_PREFIX_KEY + clientIdentifier).delete();
+            if (list.size() > 0) {
+                redissonClient.getBucket(BootstrapKey.Redis.REDIS_SESSION_MESSAGE_PREFIX_KEY + clientIdentifier).delete();
+            }
+            return resList;
+        } catch (Exception e) {
+            log.error("getSessionMessage error clientIdentifier:{}", clientIdentifier, e);
+            return Collections.emptyList();
         }
-        return resList;
     }
 
     @Override
     public void saveSessionMessage(SessionMessage sessionMessage) {
         String topic = sessionMessage.getTopic();
-        String clientId = sessionMessage.getClientIdentifier();
+        String clientIdentifier = sessionMessage.getClientIdentifier();
         int qos = sessionMessage.getQos();
         boolean retain = sessionMessage.isRetain();
         byte[] body = sessionMessage.getBody();
 
-        SessionMessageEntity sessionMessageEntity = SessionMessageEntity.builder()
-                .topic(topic)
-                .clientId(clientId)
-                .qos(qos)
-                .body(body)
-                .retain(retain)
-                .createTime(new Date()).build();
+        try {
+            SessionMessageEntity sessionMessageEntity = SessionMessageEntity.builder()
+                    .topic(topic)
+                    .clientId(clientIdentifier)
+                    .qos(qos)
+                    .body(body)
+                    .retain(retain)
+                    .createTime(new Date()).build();
 
-        RList<SessionMessageEntity> list = redissonClient.getList(BootstrapKey.Redis.REDIS_SESSION_MESSAGE_PREFIX_KEY + clientId);
-        list.add(sessionMessageEntity);
+            RList<SessionMessageEntity> list = redissonClient.getList(BootstrapKey.Redis.REDIS_SESSION_MESSAGE_PREFIX_KEY + clientIdentifier);
+            list.add(sessionMessageEntity);
+        } catch (Exception e) {
+            log.error("saveSessionMessage error message: {}", clientIdentifier, e);
+        }
     }
 
     @Override
     public void saveRetainMessage(RetainMessage retainMessage) {
-        String topic = retainMessage.getTopic();
-        int qos = retainMessage.getQos();
+        try {
+            String topic = retainMessage.getTopic();
+            int qos = retainMessage.getQos();
 
-        if (retainMessage.getBody() == null || retainMessage.getBody().length == 0) {
-            redissonClient.getBucket(BootstrapKey.Redis.REDIS_RETAIN_MESSAGE_PREFIX_KEY + topic).delete();
-        } else {
-            Date date = new Date();
-            RetainMessageEntity retainMessageEntity = RetainMessageEntity.builder()
-                    .topic(topic)
-                    .qos(qos)
-                    .body(retainMessage.getBody())
-                    .createTime(date)
-                    .updateTime(date).build();
+            if (retainMessage.getBody() == null || retainMessage.getBody().length == 0) {
+                redissonClient.getBucket(BootstrapKey.Redis.REDIS_RETAIN_MESSAGE_PREFIX_KEY + topic).delete();
+            } else {
+                Date date = new Date();
+                RetainMessageEntity retainMessageEntity = RetainMessageEntity.builder()
+                        .topic(topic)
+                        .qos(qos)
+                        .body(retainMessage.getBody())
+                        .createTime(date)
+                        .updateTime(date).build();
 
-            RBucket<RetainMessageEntity> bucket = redissonClient.getBucket(BootstrapKey.Redis.REDIS_RETAIN_MESSAGE_PREFIX_KEY + topic);
-            bucket.set(retainMessageEntity);
+                RBucket<RetainMessageEntity> bucket = redissonClient.getBucket(BootstrapKey.Redis.REDIS_RETAIN_MESSAGE_PREFIX_KEY + topic);
+                bucket.set(retainMessageEntity);
+            }
+        } catch (Exception e) {
+            log.error("saveRetainMessage error message: {}", retainMessage, e);
         }
     }
 
     @Override
     public List<RetainMessage> getRetainMessage(String topic) {
-        RBucket<RetainMessageEntity> bucket = redissonClient.getBucket(BootstrapKey.Redis.REDIS_RETAIN_MESSAGE_PREFIX_KEY + topic);
-        RetainMessageEntity retainMessageEntity = bucket.get();
-
-        RetainMessage retainMessage = Optional.ofNullable(retainMessageEntity).map(item ->
-                RetainMessage.builder()
-                        .topic(item.getTopic())
-                        .qos(item.getQos())
-                        .body(item.getBody())
-                        .build()
-        ).orElse(null);
-
-        if (retainMessage != null) {
-            List<RetainMessage> list = new ArrayList<>();
-            list.add(retainMessage);
+        try {
+            RKeys keys = redissonClient.getKeys();
+            Iterable<String> foundedKeys = keys.getKeysByPattern(BootstrapKey.Redis.REDIS_RETAIN_MESSAGE_PREFIX_KEY + "*");
+            List<RetainMessage> list = StreamSupport.stream(foundedKeys.spliterator(), false)
+                    .map(tp -> tp.replaceAll(BootstrapKey.Redis.REDIS_RETAIN_MESSAGE_PREFIX_KEY, ""))
+                    .filter(tp -> tp.matches(TopicRegexUtils.regexTopic(topic)))
+                    .map(tp -> {
+                        RBucket<RetainMessageEntity> bucket = redissonClient.getBucket(BootstrapKey.Redis.REDIS_RETAIN_MESSAGE_PREFIX_KEY + tp);
+                        RetainMessageEntity retainMessageEntity = bucket.get();
+                        return Optional.ofNullable(retainMessageEntity).map(item ->
+                                RetainMessage.builder()
+                                        .topic(item.getTopic())
+                                        .qos(item.getQos())
+                                        .body(item.getBody())
+                                        .build()
+                        ).orElse(null);
+                    })
+                    .collect(Collectors.toList());
             return list;
+        } catch (Exception e) {
+            log.error("getRetainMessage error topic: {}", topic, e);
         }
-        return null;
+        return Collections.emptyList();
     }
 
 }
