8082e9ba5e56de5c34fa8d1c16c4d1178a9d03df
luxurong <luxurong520@hellobike.com>
2021-06-24 21:43:45 +0800

    resources

--- smqtt-bootstrap/src/main/resources/static/static/js/chunk-2d0f0f76.dfec53f0.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f0f76"],{"9f2e":function(n,c,w){"use strict";w.r(c);var a=w("bc84");c["default"]=a["a"]}}]);
--- smqtt-core/src/main/java/io/github/quickmsg/core/http/actors/resource/IndexResourceActor.java
+++ smqtt-core/src/main/java/io/github/quickmsg/core/http/actors/resource/IndexResourceActor.java
@@ -3,6 +3,7 @@ package io.github.quickmsg.core.http.actors.resource;
 import io.github.quickmsg.common.http.HttpActor;
 import io.github.quickmsg.common.annotation.Router;
 import io.github.quickmsg.common.enums.HttpType;
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import reactor.netty.http.server.HttpServerRequest;
 import reactor.netty.http.server.HttpServerResponse;
@@ -15,13 +16,15 @@ import java.nio.file.Paths;
  * @author luxurong
  */
 @Router(type = HttpType.GET, value = "/smqtt/admin")
+@Slf4j
 public class IndexResourceActor implements HttpActor {
 
     private final static String DEFAULT_STATIC_PATH = "static/";
 
     @Override
     public Publisher<Void> doRequest(HttpServerRequest request, HttpServerResponse response) {
-        String path= this.getClass().getResource("/")+DEFAULT_STATIC_PATH+"index.html";
-        return response.sendFile(Paths.get(URI.create(path))).then();
+        String path=DEFAULT_STATIC_PATH+"index.html";
+        log.info("index path {}",path);
+        return response.sendFile(Paths.get(path)).then();
     }
 }
