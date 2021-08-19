a9af5228c188ca38cf0b15ea26b13f1245edab08
luxurong <1074455781@qq.com>
2021-08-19 22:45:42 +0800

    port

--- smqtt-bootstrap/src/main/java/io/github/quickmsg/config/StarterConfig.java
+++ smqtt-bootstrap/src/main/java/io/github/quickmsg/config/StarterConfig.java
@@ -1,5 +1,6 @@
 package io.github.quickmsg.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -7,4 +8,43 @@ import lombok.Data;
  */
 @Data
 public class StarterConfig {
+
+
+    @JsonProperty("smqtt")
+    private SmqttConfig smqttConfig;
+
+    @Data
+    public static class SmqttConfig {
+
+        @JsonProperty("tcp")
+        private TcpConfig tcpConfig;
+
+        @JsonProperty("http")
+        private HttpConfig httpConfig;
+
+        @JsonProperty("ws")
+        private WebsocketConfig websocketConfig;
+
+
+        @JsonProperty("cluster")
+        private ClusterConfig clusterConfig;
+
+    }
+
+    @Data
+    private static class TcpConfig {
+
+        private String port;
+    }
+
+    private static class HttpConfig {
+    }
+
+    private static class WebsocketConfig {
+    }
+
+    private static class ClusterConfig {
+    }
+
+
 }
