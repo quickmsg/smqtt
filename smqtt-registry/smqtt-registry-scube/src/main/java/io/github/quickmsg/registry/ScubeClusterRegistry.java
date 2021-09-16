80b89eb73b3bb2f1dcd3ad6aba696e82d3c21db8
luxurong <luxurong520@hellobike.com>
2021-09-16 20:57:46 +0800

    reactor pom update

--- smqtt-registry/smqtt-registry-scube/src/main/java/io/github/quickmsg/registry/ScubeClusterRegistry.java
+++ smqtt-registry/smqtt-registry-scube/src/main/java/io/github/quickmsg/registry/ScubeClusterRegistry.java
@@ -11,8 +11,10 @@ import io.scalecube.cluster.ClusterMessageHandler;
 import io.scalecube.cluster.Member;
 import io.scalecube.cluster.membership.MembershipEvent;
 import io.scalecube.cluster.transport.api.Message;
+import io.scalecube.cluster.transport.api.Transport;
+import io.scalecube.cluster.transport.api.TransportConfig;
+import io.scalecube.cluster.transport.api.TransportFactory;
 import io.scalecube.net.Address;
-import io.scalecube.transport.netty.tcp.TcpTransportFactory;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -48,7 +50,6 @@ public class ScubeClusterRegistry implements ClusterRegistry {
                     }
                     return opts;
                 })
-                .transportFactory(TcpTransportFactory::new)
                 .transport(transportConfig -> transportConfig.port(clusterConfig.getPort()))
                 .membership(opts -> opts.seedMembers(Arrays.stream(clusterConfig
                         .getUrl()
