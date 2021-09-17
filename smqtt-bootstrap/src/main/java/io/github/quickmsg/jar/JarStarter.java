e63e5b56e22b11cc4564eed6b6092c209455a56d
1091927336@qq.com <1091927336@qq.com>
2021-09-17 18:26:57 +0800

    添加启动打印banner

--- smqtt-bootstrap/src/main/java/io/github/quickmsg/jar/JarStarter.java
+++ smqtt-bootstrap/src/main/java/io/github/quickmsg/jar/JarStarter.java
@@ -1,9 +1,10 @@
 package io.github.quickmsg.jar;
 
 import io.github.quickmsg.AbstractStarter;
+import io.github.quickmsg.common.utils.BannerUtils;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Properties;
+import java.io.IOException;
 
 /**
  * @author luxurong
@@ -11,12 +12,16 @@ import java.util.Properties;
 @Slf4j
 public class JarStarter extends AbstractStarter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BannerUtils.banner();
+
         log.info("JarStarter start args {}", String.join(",", args));
         if (args.length > 0) {
             start(args[0]);
         } else {
             start(null);
         }
+
     }
 }
---
+++ smqtt-common/src/main/java/io/github/quickmsg/common/utils/BannerUtils.java
@@ -0,0 +1,38 @@
+package io.github.quickmsg.common.utils;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+/**
+ * banner
+ *
+ * @author zhaopeng
+ * @date 2021/09/17
+ */
+@Slf4j
+public class BannerUtils {
+
+    /**
+     * 打印banner
+     */
+    public static void banner() {
+        try (InputStream is = BannerUtils.class.getResourceAsStream("/banner.txt")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            log.error("banner file not exists");
+        }
+    }
+
+
+}
+
+
