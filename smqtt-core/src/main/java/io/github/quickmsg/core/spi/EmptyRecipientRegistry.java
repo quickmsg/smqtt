96b7e77af80664d1222b5739220a42fa46ea5940
1091927336@qq.com <1091927336@qq.com>
2021-10-09 14:39:38 +0800

    修复抽象类参数不一致问题

--- smqtt-core/src/main/java/io/github/quickmsg/core/spi/EmptyRecipientRegistry.java
+++ smqtt-core/src/main/java/io/github/quickmsg/core/spi/EmptyRecipientRegistry.java
@@ -2,8 +2,8 @@ package io.github.quickmsg.core.spi;
 
 import io.github.quickmsg.common.channel.MqttChannel;
 import io.github.quickmsg.common.enums.ChannelStatus;
+import io.github.quickmsg.common.message.HeapMqttMessage;
 import io.github.quickmsg.common.message.RecipientRegistry;
-import io.netty.handler.codec.mqtt.MqttPublishMessage;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -14,7 +14,7 @@ public class EmptyRecipientRegistry implements RecipientRegistry {
 
 
     @Override
-    public void accept(MqttChannel mqttChannel, MqttPublishMessage publishMessage) {
+    public void accept(MqttChannel mqttChannel, HeapMqttMessage publishMessage) {
         if(log.isDebugEnabled()){
             log.debug("RecipientRegistry message channel {} message {}",mqttChannel,publishMessage);
         }
