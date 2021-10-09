dcbd5f00a5a07c754f37a4532023df170ba76dc6
luxurong <luxurong520@hellobike.com>
2021-10-09 16:18:22 +0800

    RecipientRegistry update

--- smqtt-core/src/main/java/io/github/quickmsg/core/spi/EmptyRecipientRegistry.java
+++ smqtt-core/src/main/java/io/github/quickmsg/core/spi/EmptyRecipientRegistry.java
@@ -2,6 +2,7 @@ package io.github.quickmsg.core.spi;
 
 import io.github.quickmsg.common.channel.MqttChannel;
 import io.github.quickmsg.common.enums.ChannelStatus;
+import io.github.quickmsg.common.message.HeapMqttMessage;
 import io.github.quickmsg.common.message.RecipientRegistry;
 import io.netty.handler.codec.mqtt.MqttPublishMessage;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +15,7 @@ public class EmptyRecipientRegistry implements RecipientRegistry {
 
 
     @Override
-    public void accept(MqttChannel mqttChannel, MqttPublishMessage publishMessage) {
+    public void accept(MqttChannel mqttChannel, HeapMqttMessage publishMessage) {
         if(log.isDebugEnabled()){
             log.debug("RecipientRegistry message channel {} message {}",mqttChannel,publishMessage);
         }
