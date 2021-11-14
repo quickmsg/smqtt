88a4b97b26bdda69407241bc0403daa224a7ac7e
Easy <1091927336@qq.com>
2021-11-14 13:03:00 +0800

    add influxdb

--- smqtt-metric/smqtt-metric-influx1/src/main/java/io/github/quickmsg/metric/Influx1Metric.java
+++ smqtt-metric/smqtt-metric-influx1/src/main/java/io/github/quickmsg/metric/Influx1Metric.java
@@ -70,7 +70,7 @@ public class Influx1Metric implements Metric {
         new ProcessorMetrics().bindTo(Metrics.globalRegistry);
         new JvmThreadMetrics().bindTo(Metrics.globalRegistry);
 
-        map.put(CounterEnum.CONNETC_COUNTER, ConnectCounter.CONNECT_SIZE_COUNTER_INSTANCE);
+        map.put(CounterEnum.CONNECT_COUNTER, ConnectCounter.CONNECT_SIZE_COUNTER_INSTANCE);
         map.put(CounterEnum.TOPIC_COUNTER, TopicCounter.TOPIC_COUNTER_INSTANCE);
     }
 
