489da51c3ba19549d042d4a158e03179417dd7de
luxurong <luxurong520@hellobike.com>
2021-12-02 21:44:55 +0800

    pom

---
+++ smqtt-metric/smqtt-metric-prometheus/src/main/java/io/github/quickmsg/metric/PrometheusMetricBean.java
@@ -0,0 +1,25 @@
+package io.github.quickmsg.metric;
+
+import io.github.quickmsg.common.metric.MetricBean;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+
+/**
+ * @author luxurong
+ */
+public class PrometheusMetricBean implements MetricBean {
+
+    public final PrometheusMeterRegistry prometheusMeterRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+
+    @Override
+    public MetricBean Close() {
+        prometheusMeterRegistry.close();
+        return this;
+    }
+
+    @Override
+    public MeterRegistry getMeterRegistry() {
+        return this.prometheusMeterRegistry;
+    }
+}
